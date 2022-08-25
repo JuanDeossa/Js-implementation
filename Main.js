f2e184d817bccdd1aa7ca3e11ec794a004ebb00b
JuanDeossa <juandeossa.nc@gmail.com>
2022-08-25 15:04:58 -0500

    Definition of 3 querySelectors and .isHidden class

---
+++ Main.js
@@ -0,0 +1,4 @@
+let emailMenuLink = document.querySelector(".navbar-email");
+let bgrMenuLink = document.querySelector(".menu");
+let shoppingCarMenuLink = document.querySelector(".navbar-shopping-cart");
+
